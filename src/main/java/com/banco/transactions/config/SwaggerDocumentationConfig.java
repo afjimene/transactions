26752942c48d8aa5146562d37834e18eddbdabeb
Andres Jimenez <andres.f.jimenez@accenture.com>
2019-08-05 10:23:36 -0500

    Swagger documentation

---
+++ src/main/java/com/banco/transactions/config/SwaggerDocumentationConfig.java
@@ -0,0 +1,30 @@
+package com.banco.transactions.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerDocumentationConfig {
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Transactions").description("API Transactions").license("")
+                .licenseUrl("http://unlicense.org").termsOfServiceUrl("").version("1.0.0")
+                .contact(new Contact("Andres Jimenez", "https://github.com/afjimene", "afjimene@gmail.com")).build();
+    }
+
+    @Bean
+    public Docket customImplementation() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.banco.transactions"))
+                .paths(PathSelectors.any()).build().apiInfo(apiInfo());
+    }
+}
